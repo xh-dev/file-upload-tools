0e7efa01d858b99abd92bb4a25a41a385dbc7efb
xeth hung <adamhung12@gmail.com>
2022-08-31 22:05:20 +0800

    support dynamic response

--- src/main/java/dev/xethh/tools/controller/UploadController.java
+++ src/main/java/dev/xethh/tools/controller/UploadController.java
@@ -1,5 +1,8 @@
 package dev.xethh.tools.controller;
 
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import dev.xethh.utils.WrappedResult.wrappedResult.WrappedResult;
 import io.vavr.Tuple;
 import io.vavr.Tuple2;
@@ -9,6 +12,7 @@ import lombok.Builder;
 import lombok.Data;
 import me.xethh.utils.functionalPacks.Scope;
 import org.apache.commons.codec.digest.DigestUtils;
+import org.springframework.http.MediaType;
 import org.springframework.http.codec.multipart.FilePart;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -33,25 +37,39 @@ import java.util.function.Function;
 @RequestMapping("api/upload")
 public class UploadController {
 
+    @JsonTypeInfo(
+            use = JsonTypeInfo.Id.NAME,
+            property = "type")
+    @JsonSubTypes({
+            @JsonSubTypes.Type(value = PostResponse.class, name = "PostResponse"),
+            @JsonSubTypes.Type(value = ErrorResponse.class, name = "ErrorResponse")
+    })
+    public interface Response {
+    }
+
     @Data
-    public static class PostResponse{
+    public static class PostResponse implements Response {
         private Boolean success = true;
-        private String msg;
         private String fileName;
         private Long size;
         private String hash;
     }
 
+    @Data
+    public static class ErrorResponse implements Response {
+        private Boolean success = true;
+        private String msg;
+    }
+    Function<UUID, File> fileSupplier = (uuid ->
+            Try.of(() -> new File("./target/uploads").mkdirs())
+                    .map(it ->
+                            new File(String.format("./target/uploads/%s", uuid.toString()))
+                    ).get()
+    );
 
-    Function<UUID, File> fileSupplier = (uuid -> {
-        new File("./target/uploads").mkdirs();
-        return new File(String.format("./target/uploads/%s", uuid.toString()));
-    });
-
-    @PostMapping
-    public Mono<PostResponse> post(
-            @RequestPart("hash") String hash,
-            @RequestPart("size") String size,
+    @PostMapping(produces = {MediaType.APPLICATION_JSON_VALUE})
+    public Mono<Response> post(
+            @RequestPart("code") String code,
             @RequestPart("file") Mono<FilePart> fileMono
     ) {
         File file = fileSupplier.apply(UUID.randomUUID());
@@ -74,24 +92,16 @@ public class UploadController {
                             ;
                 })
                 .map(fileName -> {
-                    if (Long.parseLong(size) != file.length()) {
-                        RuntimeException ex = new RuntimeException(String.format("File size %d not match pushed file size %s", file.length(), size));
-                        ex.printStackTrace();
-                        throw new RuntimeException("Meta data not match", ex);
-                    }
                     String computedHash = HexFormat.of().formatHex(digestSha256.digest());
-                    if(!computedHash.equals(hash)){
-                        RuntimeException ex = new RuntimeException(String.format("File has %s not match pushed file hash %s", computedHash, hash));
-                        throw new RuntimeException("Meta data not match", ex);
-                    }
-                    return Scope.apply(new PostResponse(), postResponse1 -> {
+                    return (Response) Scope.apply(new PostResponse(), postResponse1 -> {
                         postResponse1.setFileName(fileName);
                         postResponse1.setSize(file.length());
                         postResponse1.setHash(computedHash);
                     });
                 })
-                .onErrorResume(it -> Mono.just(Scope.apply(new PostResponse(), postResponse1 -> {
+                .onErrorResume(it -> Mono.just(Scope.apply(new ErrorResponse(), postResponse1 -> {
                     postResponse1.setMsg(it.getMessage());
-                })));
+                })))
+                ;
     }
 }
